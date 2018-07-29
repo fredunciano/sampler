655d93b7f10ccb32b625b57717dd417bfd60c24d
fredunciano <41849058+fredunciano@users.noreply.github.com>
2018-07-29 13:30:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+</!DOCTYPE html>
+<html>
+<head>
+
+	<title>Sample</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+</head>
+<body>
+	<h1>Hello</h1>
+	<?php
+  echo "THERE";
+  ?>
+</body>
+</html>
